ec257821b061701e4c51b7fc5acffa6a4be1e04e
sray <sabyaxsorryi@gmail.com>
2026-06-28 14:32:59 +0530

    modified code as per senior's review

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -3,8 +3,9 @@ package com.qa.opencart.pages;
 public class DemoPage {
 	
 	int i = 10;
+	int j = 20;
 	public void grtPage() {
-		System.out.println("This is demo page : " + i );
+		System.out.println("This is demo page : " + i + j);
 	}
 
 }
