91f397534b8a6b143e5f6fbd67aa182aacba922e
sabyray-in <sabyasachi.web@gmail.com>
2026-06-28 15:47:22 +0530

    Add TestPage class with a method to sum two integers

---
+++ src/main/java/com/qa/opencart/pages/TestPage.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class TestPage {
+	int a = 10;
+	int b = 20;
+	public int getTetPage() {
+		System.out.println("This is demo page : " + a + b);
+		return a+b;
+	}
+}
